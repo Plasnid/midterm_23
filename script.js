b2eb2aeba64c1cd28505347c2896833307311ff5
Plasnid <stuart@sackler.ca>
2023-02-21 22:36:12 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+/**
+ * * Midterm 2601 - Winter 2023 - 60 Marks Total
+ * 
+ * 
+ * * 1.  Link the script.js file to the html in the head tag - 5 marks
+ */
+
+/**
+ * * 2. Declaring variables - 10 marks
+ * 
+ * * name and set the following variables
+ * 
+ * * a string containing the name of your favourite song
+ * * a string containing the artist who sings your favourite song
+ * * a template literal containing the previous two variables
+ * 
+ * * a number containing the speed limit of a street
+ * * an boolean about a topic of your choice
+ * * an array listing items used in a hobby you partake in
+ * * an object describing your favourite food
+ */
+
+/**
+ * * 3. Function declarations and string literals - 20 marks
+ * * 
+ * * Create a function declaration that takes in two parameters
+ * *
+ * * Use the two parameters to fill in the following sentence using a template literal
+ * 
+ * * "My favourite ______ is called _____!"
+ * 
+ * * Have the function declaration return the sentence and write it to a P tag that you make with JavaScript
+ * * Put the created p tag containing the sentence into the body of your HTML using DOM Manipulation
+ * * Create a style in the css file and use a classList command to add that style to the sentence
+ * * Make the style change the font size, the font colour, and the backgound colour
+ */
+
+/**
+ * * 4. Arrays and Looping - 15 Marks
+ * 
+ * * Create an array about 1 of the following topics:
+ * * -foods
+ * * -world cities
+ * * -animals
+ * * -tv shows
+ * 
+ * * Be sure to have at least 5 entries in your array.  Each entry must be a string.
+ * * Create a for loop and add each entry to the screen(in a p tag) and the console log. 
+ */
+
+/**
+ * * 5. Decisions - 10 Marks
+ * 
+ * * Create a function declaration containing an if statement about what to eat.  
+ * 
+ * * Using an if/else if/else, create separate blocks for answers for each of the following
+ * * - carnavores(meat eaters)
+ * * - herbivores(plant eaters)
+ * * - pescavores(fish eaters)
+ * * - omnivores(eats anything)
+ * 
+ * * Run the function and console log the phrase it chooses
+ */
